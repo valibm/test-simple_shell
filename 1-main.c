cbc4b383278213f56246a486e515f9b902cd1996
root <valibm>
2023-12-25 00:13:41 -0800

    Added initial files

---
+++ 1-main.c
@@ -0,0 +1,50 @@
+#include <string.h>
+
+/**
+ * main - open shell, project base
+ * Return: int
+ */
+
+int main(void)
+{
+	char *buffer = NULL, **args;
+	size_t read_s = 0;
+	ssize_t buff_s = 0;
+	int exit_stat = 0;
+
+	while (1)
+	{
+		if (isatty(0))
+			printf("hsh$ ");
+
+		buff_s = getline(&buffer, &read_s, stdin);
+		if (buff_s == -1 || strcmp("exit\n", buffer) == 0)
+		{
+			free(buffer);
+			break;
+		}
+		buffer[buff_s - 1] = '\0';
+
+		if (strcmp("env", buffer) == 0)
+		{
+			_env();
+			continue;
+		}
+
+		if (check_buff(buffer) == 1)
+		{
+			exit_status = 0;
+			continue;
+		}
+
+		args = split(buffer, " ");
+		args[0] = find_file(args[0]);
+
+		if (args[0] != NULL)
+			exit_status = execute(args);
+		else
+			perror("Error");
+		free(args);
+	}
+	return (exit_status);
+}
---
+++ environment.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _getenv - Get environment variables.
+ * @env: nvironment variable.
+ * Return: Contents of environment variable.
+ */
+
+char *_getenv(char *env)
+{
+	int i = 0, j;
+	int status;
+
+	while (environment[i])
+	{
+		status = 1;
+
+		for (j = 0; environment[i][j] != '='; j++)
+		{
+			if (environment[i][j] != env[j])
+				status = 0;
+		}
+		if (status == 1)
+			break;
+		i++;
+	}
+	return (&environment[i][j + 1]);
+}
+
+/**
+ * _env - It's a function that prints the environment.
+ *
+ * Return: Void.
+ */
+void _env(void)
+{
+	int i = 0;
+
+	while (environment[i])
+	{
+		printf("%s\n", environment[i]);
+		i++;
+	}
+}
